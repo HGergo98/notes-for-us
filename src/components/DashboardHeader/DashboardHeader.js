28ccf82ec4dcbf4949fe95ff9c98d4b01b6bd338
HGergo <gergo.hegyi@calv.us>
2023-02-18 17:15:39 +0100

    implementing auth

--- src/components/DashboardHeader/DashboardHeader.js
+++ src/components/DashboardHeader/DashboardHeader.js
@@ -1,15 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
+import { useSendLogoutMutation } from "../../app/api/authApiSlice";
+
+const DASHBOARD_REGEX = /^\/dashboard(\/)?$/;
+const NOTES_REGEX = /^\/dashboard\/notes?$/;
+const USERS_REGEX = /^\/dashboard\/users?$/;
 
 const DashboardHeader = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("/");
+    }
+  }, [isSuccess, navigate]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error: {error.data?.message}</p>;
+  }
+
+  let dashboardHeaderContainerClass = null;
+
+  if (
+    !DASHBOARD_REGEX.test(pathname) ||
+    NOTES_REGEX.test(pathname) ||
+    USERS_REGEX.test(pathname)
+  ) {
+    dashboardHeaderContainerClass = "dash-header__container--small";
+  }
+
+  const logoutButton = (
+    <button className="icon-button" title="Logout" onClick={sendLogout}>
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  );
+
   return (
     <header className="dash-header">
-      <div className="dash-header__container">
+      <div
+        className={`dash-header__container ${dashboardHeaderContainerClass}`}
+      >
         <Link to="/dashboard">
           <h1 className="dash-header__title">Notes for us.</h1>
         </Link>
         <nav className="dash-header__nav">
-          {/* add nav buttons later */}
+          {/* add more buttons later */}
+          {logoutButton}
         </nav>
       </div>
     </header>
--- src/pages/authSpecific/Login.js
+++ src/pages/authSpecific/Login.js
@@ -1,8 +1,99 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { setCredentials } from "../../app/api/authSlice";
+import { useLoginMutation } from "../../app/api/authApiSlice";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userRef = useRef();
+  const errorRef = useRef();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const errClass = errMsg ? "errmsg" : "offscreen";
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, password]);
+
+  const handleUserInput = (e) => setUsername(e.target.value);
+  const handlePwdInput = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { accessToken } = await login({ username, password }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      setUsername("");
+      setPassword("");
+      navigate("/dashboard");
+    } catch (error) {
+      if (!error.status) {
+        setErrMsg("No Server Response");
+      } else if (error.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else {
+        setErrMsg(error.data?.message);
+      }
+      errorRef.current.focus();
+    }
+  };
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <h1>Login</h1>
+    <section className="public">
+      <header>
+        <h1>User Login</h1>
+      </header>
+      <main className="login">
+        <p ref={errorRef} className={errClass} aria-live="assertive">
+          {errMsg}
+        </p>
+
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            className="form__input"
+            type="text"
+            id="username"
+            ref={userRef}
+            value={username}
+            onChange={handleUserInput}
+            autoComplete="off"
+            required
+          />
+
+          <label htmlFor="password">Password:</label>
+          <input
+            className="form__input"
+            type="password"
+            id="password"
+            onChange={handlePwdInput}
+            value={password}
+            required
+          />
+          <button className="form__submit-button">Sign In</button>
+        </form>
+      </main>
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
+    </section>
   );
 };
 
